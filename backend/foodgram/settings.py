c1e9e5b155b1ab2014785645d3af22f6e3eac8e0
laser54 <Z6X84xU4Ir2U>
2022-10-09 21:05:47 +0700

    after review

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -153,7 +154,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 
 CORS_ORIGIN_ALLOW_ALL = True
-# CSRF_TRUSTED_ORIGINS = ["http://http://51.250.34.40"]
+CSRF_TRUSTED_ORIGINS = ['http://http://51.250.34.40']
 # CORS_URLS_REGEX = r'^/api/.*$'
 # CORS_ALLOW_ALL_ORIGINS = True
 CSRF_COOKIE_SECURE = False
