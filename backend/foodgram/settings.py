e17dd2994460616948e429a40ea03bbb1ef3e916
laser54 <Z6X84xU4Ir2U>
2022-10-09 19:30:45 +0700

    after review

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -155,5 +155,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 
 CORS_ORIGIN_ALLOW_ALL = True
-CSRF_TRUSTED_ORIGINS = ['*']
-# CORS_URLS_REGEX = r'^/api/.*$'
+CSRF_TRUSTED_ORIGINS = ["http://http://51.250.34.40"]
+CORS_URLS_REGEX = r'^/api/.*$'
+CORS_ALLOW_ALL_ORIGINS = True
